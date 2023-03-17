1a0ae2757e06b42d6881f224716767710cad9cad
a-colabella <acolabella11@gmail.com>
2023-03-16 19:28:16 -0600

    added comments

--- src/blockchain.js
+++ src/blockchain.js
@@ -2,17 +2,41 @@ const SHA256 = require('crypto-js/sha256');
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
+/**
+ * Defines a Transaction which consists of
+ * fromAddress: the sender
+ * toAddress: the receiver
+ * amount: amount being transfered
+ */
 class Transaction {
+    /**
+     * 
+     * @param {string} fromAddress 
+     * @param {string} toAddress 
+     * @param {number} amount 
+     */
     constructor(fromAddress, toAddress, amount) {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
     }
 
+    /**
+     * Creates SHA256 hash of transaction.
+     * 
+     * @returns {string}
+     */
     calculateHash() {
         return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
     }
 
+    /**
+     * Signs a transaction with the Elliptic keypair
+     * that contains the user's private key.
+     * Signature is stored inside the transaction object and stored
+     * on chain.
+     * @param {string} signingKey 
+     */
     signTransaction(signingKey) {
         if (signingKey.getPublic('hex') !== this.fromAddress) {
             throw new Error('You cannot sign transactions for other wallets.');
@@ -23,7 +47,12 @@ class Transaction {
         this.signature = sig.toDER('hex');
     }
 
+    /**
+     * Checks if signature is valid. Uses fromAddress public key.
+     * @returns {boolean}
+     */
     isValid() {
+        // We assume null fromAddress means it was a mining reward.
         if (this.fromAddress === null) return true;
 
         if (!this.signature || this.signature.length === 0) {
@@ -35,7 +64,21 @@ class Transaction {
     }
 }
 
+/**
+ * Defines a single block on the blockchain.
+ * A Block is comprised of
+ * timestamp: when the block was created.
+ * transactions: a list of transactions between parties
+ * hash: a calculated identifier for the block
+ * previousHash: a reference to the previous block in the chain
+ */
 class Block {
+    /**
+     * 
+     * @param {Date} timestamp 
+     * @param {Transaction[]} transactions 
+     * @param {string} previousHash 
+     */
     constructor(timestamp, transactions, previousHash = '') {
         this.timestamp = timestamp;
         this.transactions = transactions;
@@ -44,10 +87,22 @@ class Block {
         this.nonce = 0;
     }
 
+    /**
+     * Returns the SHA256 hash of this block.
+     * Calculated by processing all of the data in the block
+     * with SHA256 algorithm and converting to a string.
+     * @returns {string}
+     */
     calculateHash() {
         return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
     }
 
+    /**
+     * Mines a block by changing the 'nonce' value
+     * until the hash of the block begins with
+     * the same number of 0s equal to the difficulty number.
+     * @param {number} difficulty 
+     */
     mineBlock(difficulty) {
         while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
             this.nonce++;
@@ -57,6 +112,13 @@ class Block {
         console.log("Block mined: " + this.hash);
     }
 
+    /**
+     * Validates all transactions in this block
+     * checking signature and hash. Returns true
+     * if all transactions are valid. Returns false
+     * if a transaction is invalid.
+     * @returns {boolean}
+     */
     hasValidTransactions() {
         for(const tx of this.transactions) {
             if(!tx.isValid()) {
@@ -68,6 +130,13 @@ class Block {
     }
 }
 
+/**
+ * Defines the blockchain which consists of
+ * chain: an array of Blocks
+ * difficulty: the difficulty level to mine a new block
+ * pendingTransactions: an array of Transactions that have yet to be mined.
+ * miningReward: the amount of tokens rewarded to user that mines a block.
+ */
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
@@ -76,14 +145,33 @@ class Blockchain {
         this.miningReward = 50;
     }
 
+    /**
+     * The first block must be hardcoded onto the chain
+     * to initialize the chain. References March 15, 2023
+     * as the date when the chain was first initialized.
+     * @returns {Block}
+     */
     createGenesisBlock() {
         return new Block(Date.parse("2023-03-15"), "NYT: Bank Fears Go Global, Sending Shudder Through Markets", "0");
     }
 
+    /**
+     * Returns the latest block on the chain.
+     * Used for creating a new block and finding the hash
+     * of the previous block.
+     * @returns {Block}
+     */
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
+    /**
+     * Puts all pending transactions into a new
+     * Block and begins mining. This Block will
+     * also include a transaction for sending the mining
+     * reward to the given address.
+     * @param {string} miningRewardAddress 
+     */
     minePendingTransactions(miningRewardAddress) {
         const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
         this.pendingTransactions.push(rewardTx);
@@ -97,19 +185,59 @@ class Blockchain {
         this.pendingTransactions = [];
     }
 
+    /**
+     * Adds a new transaction to the list of pending
+     * transactions (to be added for the next mined block).
+     * This function will verify that the transaction is signed and valid.
+     * @param {Transaction} transaction 
+     */
     addTransaction(transaction) {
-
+        // Cannot send a transaction without a from and to address
         if (!transaction.fromAddress || !transaction.toAddress) {
             throw new Error('Transaction must include from and to address');
         }
 
+        // Verify validity of transaction
         if (!transaction.isValid()) {
             throw new Error('Cannot add invalid transaction to chain');
+        }
+
+        // Ensure that amount sent is not greater than existing balance
+        const walletBalance = this.getBalanceOfAddress(transaction.fromAddress);
+        if (walletBalance < transaction.amount) {
+            throw new Error('Not enough balance');
+        }
+
+        // Get all other pending transactions for the "from" wallet
+        const pendingTxForWallet = this.pendingTransactions.filter(
+            tx => tx.fromAddress === transaction.fromAddress
+        );
+  
+        // If the wallet has more pending transactions, calculate the total amount
+        // of spend coins so far. If this exceeds the balance, we refuse to add this
+        // transaction.
+        if (pendingTxForWallet.length > 0) {
+            const totalPendingAmount = pendingTxForWallet
+            .map(tx => tx.amount)
+            .reduce((prev, curr) => prev + curr);
+    
+            const totalAmount = totalPendingAmount + transaction.amount;
+            if (totalAmount > walletBalance) {
+            throw new Error(
+                'Pending transactions for this wallet is higher than its balance.'
+            );
+            }
         }
 
         this.pendingTransactions.push(transaction);
     }
 
+    /**
+     * Returns balance of a given wallet address.
+     * 
+     * @param {string} address 
+     * @returns {number} balance of the wallet
+     */
     getBalanceOfAddress(address) {
         let balance = 0;
 
@@ -128,13 +256,27 @@ class Blockchain {
         return balance;
     }
 
+    /**
+     * Iterates over all blocks in the chain.
+     * Verifies if each block properly references the previous block
+     * and the hashes have not been tampered. Verifies the signed
+     * transactions within each block as well.
+     * 
+     * @returns {boolean}
+     */
     isChainValid() {
+        /**
+         * Confirm Genesis block hasn't been tampered with.
+         * Compare genesis of this chain with genesis from
+         * createGenesisBlock function.
+         */
         const realGenesis = JSON.stringify(this.createGenesisBlock());
 
         if(realGenesis !== JSON.stringify(this.chain[0])){
             return false;
         }
         
+        // Check all remaining blocks and confirm hashes are correct.
         for(let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
@@ -156,5 +298,6 @@ class Blockchain {
     }
 }
 
+module.exports.Block = Block;
 module.exports.Blockchain = Blockchain;
 module.exports.Transaction = Transaction;
--- src/main.js
+++ src/main.js
@@ -5,17 +5,29 @@ const ec = new EC('secp256k1');
 const myKey = ec.keyFromPrivate('21d47f378fbed211433bc1c1ee9e83b946be786bfea0a518a6db3859072e75cf');
 const myWalletAddress = myKey.getPublic('hex');
 
+// Instantiate Blockchain
 let phoneycoin = new Blockchain();
 
-const tx1 = new Transaction(myWalletAddress, 'insert public key', 10);
+// Create a transaction
+const tx1 = new Transaction(myWalletAddress, 'insert public key', 0);
+
+// Sign transaction
 tx1.signTransaction(myKey);
+
+// Add transaction to pending transactions on Chain
 phoneycoin.addTransaction(tx1);
 
+// Mine pending transactions and create a new block
 console.log('\n Starting the miner...');
 phoneycoin.minePendingTransactions(myWalletAddress);
 
+// Check miner's balance
 console.log('\nBalance of my address is ', phoneycoin.getBalanceOfAddress(myWalletAddress));
 
+// Check chain is valid
 console.log('Is chain valid? ', phoneycoin.isChainValid());
+
+// Print entire chain
+console.log(JSON.stringify(phoneycoin));
 
 
